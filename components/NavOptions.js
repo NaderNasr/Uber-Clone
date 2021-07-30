fe7437375ac23cb9105b056e81fc28ef7ebc3177
Nader Nasr <nadernasr7@gmail.com>
2021-07-30 19:41:39 -0400

    Google maps input search

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -10,7 +10,7 @@ import {
 import uberX from "../assets/UberX.png";
 import eats from "../assets/eats.png";
 import tw from "tailwind-react-native-classnames";
-import {Icon} from 'react-native-elements'
+import { Icon } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
 
 const data = [
@@ -31,18 +31,17 @@ const data = [
 ];
 
 const NavOptions = () => {
-
-    const navigation = useNavigation()
+  const navigation = useNavigation();
   return (
     <FlatList
       horizontal
       data={data}
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => (
-        <TouchableOpacity 
-        onPress={() => navigation.navigate(item.screen)}
-        
-        style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate(item.screen)}
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+        >
           <View>
             <Image
               style={{ width: 120, height: 120, resizeMode: "contain" }}
@@ -52,8 +51,11 @@ const NavOptions = () => {
             />
             <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
             <Icon
-            style={tw`p-2 bg-black rounded-full w-10 mt-4`}
-            name="arrowright" color="white" type="antdesign" />
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              name="arrowright"
+              color="white"
+              type="antdesign"
+            />
           </View>
         </TouchableOpacity>
       )}

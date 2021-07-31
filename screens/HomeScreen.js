f76bf0c66a959a7252c7dc74c2962460e10c762f
Nader Nasr <nadernasr7@gmail.com>
2021-07-30 20:02:44 -0400

    Google input complete

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,8 +4,13 @@ import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { GOOGLE_MAPS_KEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+
+    const dispatch = useDispatch()
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-7`}>
@@ -30,12 +35,25 @@ const HomeScreen = () => {
               fontSize: 18,
             },
           }}
+          onPress={(data, details = null) => {
+            //   console.log(data)
+            // console.log(details)
+            dispatch(setOrigin({
+                location: details.geometry.location,
+                description: data.description
+            }))
+            dispatch(setDestination(null))
+          }}
+          fetchDetails={true}
+          returnKeyType={"search"}
+          enablePoweredByContainer={false}
+          minLength={2}
           query={{
             key: GOOGLE_MAPS_KEY,
             language: "en",
           }}
           nearbyPlacesAPI="GooglePlacesSearch"
-          debounce={400}
+          debounce={100}
         />
         <NavOptions />
       </View>
